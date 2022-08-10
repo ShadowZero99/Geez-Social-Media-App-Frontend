80fb308a73067f7512b270df9a008a66827d1c13
Geeth <Geethmadhawa99@gmail.com>
2022-08-10 09:09:32 +0530

    start Post part

---
+++ src/FollowersData/FollowersData.js
@@ -0,0 +1,11 @@
+import i1 from "../img/i1.jpg";
+import i2 from "../img/i2.jpg";
+import i3 from "../img/i3.jpg";
+import i4 from "../img/i4.jpg";
+
+export const Followers = [
+  { name: "chirstina jenny", username: "chirsyi", img: i2 },
+  { name: "Ann Marry", username: "Ann", img: i3 },
+  { name: "Sara lowern", username: "Saraa", img: i4 },
+  { name: "Jefri Thorman", username: "Jefry", img: i1 },
+];
