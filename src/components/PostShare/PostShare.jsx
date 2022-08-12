6e37780a69df60db16026a2358079a31441db5f6
Geeth <Geethmadhawa99@gmail.com>
2022-08-12 09:22:19 +0530

    Post Share part Done

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./PostShare.css";
 import profile from "../../img/profile.jpg";
+import { UilScenery } from "@iconscout/react-unicons";
+import { UilPlayCircle } from "@iconscout/react-unicons";
+import { UilLocationPoint } from "@iconscout/react-unicons";
+import { UilSchedule } from "@iconscout/react-unicons";
+import { UilTimes } from "@iconscout/react-unicons";
 
 const PostShare = () => {
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0];
+      setImage({
+        image: URL.createObjectURL(img),
+      });
+    }
+  };
+
   return (
     <div className="PostShare">
       <img src={profile} alt="" />
+      <div>
+        <input type="text" placeholder="What's On your Mind?" />
+        <div className="postOptions">
+          <div
+            className="option"
+            style={{ color: "var(--photo)" }}
+            onClick={() => imageRef.current.click()}
+          >
+            <UilScenery />
+            Picture
+          </div>
+          <div className="option" style={{ color: "var(--video)" }}>
+            <UilPlayCircle />
+            Video
+          </div>
+          <div className="option" style={{ color: "var(--shedule)" }}>
+            <UilSchedule />
+            Time
+          </div>
+          <div className="option" style={{ color: "var(--location)" }}>
+            <UilLocationPoint />
+            Location
+          </div>
+          <button className="button ps-button">Fly</button>
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
+        </div>
+
+        {image && (
+          <div className="previewImage">
+            <UilTimes onClick={() => setImage(null)} />
+            <img src={image.image} alt="" />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
