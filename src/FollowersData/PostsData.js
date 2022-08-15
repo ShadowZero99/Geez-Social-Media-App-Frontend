e5b467ab694fc8cdb7ac89d220ffbc491185092d
Geeth <Geethmadhawa99@gmail.com>
2022-08-15 18:50:19 +0530

    post share part complete

---
+++ src/FollowersData/PostsData.js
@@ -0,0 +1,35 @@
+import i5 from "../img/i5.jpg";
+import i6 from "../img/i6.jpg";
+import i7 from "../img/i7.jpg";
+import i8 from "../img/i8.jpg";
+
+export const PostsData = [
+  {
+    img: i5,
+    name: "Ann Marry",
+    desc: "My new Adventure... with my Travel Bag...",
+    likes: 2300,
+    liked: true,
+  },
+  {
+    img: i6,
+    name: "Jefri Thorman",
+    desc: "Rain Forest camping life",
+    likes: 300,
+    liked: false,
+  },
+  {
+    img: i7,
+    name: "Chirstina jenny",
+    desc: "Summer Island",
+    likes: 200,
+    liked: true,
+  },
+  {
+    img: i8,
+    name: "Sara lowern",
+    desc: "Old ios Gold!!!",
+    likes: 1300,
+    liked: true,
+  },
+];
--- src/components/Postside/Postside.jsx
+++ src/components/Postside/Postside.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Postside.css";
 import PostShare from "../PostShare/PostShare";
+import Posts from "../Posts/Posts";
 const Postside = () => {
   return (
     <div className="Postside">
       <PostShare />
+      <Posts />
     </div>
   );
 };
