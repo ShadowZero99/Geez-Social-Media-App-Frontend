123b8fb5ddce5973f7a58ec7c761f2de71add127
Geeth <Geethmadhawa99@gmail.com>
2022-08-28 22:06:53 +0530

    user model done

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import AuthRoute from "./Routes/AuthRoute.js";
-
+import UserRoute from "./Routes/UserRoute.js";
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -23,3 +23,4 @@ mongoose
   .catch((error) => console.log(error));
 
 app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
